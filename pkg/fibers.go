ce4786fab416a3225859bcaec2c6b55280f0e4d4
jmbenlloch <info@jmbenlloch.net>
2026-02-06 16:39:56 +0100

    add missing fiber file

---
+++ pkg/fibers.go
@@ -0,0 +1,206 @@
+package decoder
+
+import (
+	"fmt"
+)
+
+func ReadFiberFEC(data []uint16, evtFormat *EventFormat, dateHeader *EventHeaderStruct, event *EventType) {
+	position := 0
+	var time int = -1
+	var current_bit int = 31
+
+	fFecId := evtFormat.FecID
+	// Fibers do not have ZS, only compression mode (same as PMTs)
+	Compression := evtFormat.ZeroSuppression
+	Baseline := evtFormat.Baseline
+	bufferSamples := evtFormat.BufferSamples
+	if evtFormat.FWVersion == 10 {
+		if evtFormat.TriggerType >= 8 {
+			bufferSamples = evtFormat.BufferSamples2
+		}
+	}
+
+	if configuration.Verbosity > 2 {
+		message := fmt.Sprintf("ReadFiberFEC: FecID=0x%02x, Compression=%t, Baseline=%t, bufferSamples=%d",
+			fFecId, Compression, Baseline, bufferSamples)
+		logger.Info(message, "fibers")
+	}
+
+	// Reading the payload
+	var nextFT int32 = -1 //At start we don't know next FT value
+	var nextFThm int32 = -1
+
+	channelMask, chPositions := fibersChannelMask(evtFormat)
+	initializeWaveforms(event.FibersLG, channelMask, bufferSamples)
+	wfPointers := computeFiberWaveformPointerArray(event.FibersLG, channelMask, chPositions)
+
+	// Write pedestal
+	if Baseline {
+		writeFiberPedestals(evtFormat, channelMask, event.FiberBaselinesLG)
+	}
+
+	for true {
+		time++
+
+		// Stop reading if we reach the last time bin of the waveform
+		if time == int(bufferSamples) {
+			break
+		}
+
+		if Compression {
+			// Skip FTm
+			if time == 0 {
+				position++
+			}
+			position = decodeChargeIndiaFiberCompressed(data, position, wfPointers,
+				&current_bit, huffmanCodesPmts, chPositions, uint32(time))
+		} else {
+			var FT int32 = int32(data[position]) & 0x0FFFF
+			position++
+
+			//If not ZS check next FT value, if not expected (0xffff) end of data
+			computeNextFThm(&nextFT, &nextFThm, evtFormat)
+			if FT != (nextFThm & 0x0FFFF) {
+				errMessage := fmt.Errorf("evt %d, fecID: %d, nextFThm != FT: 0x%04x, 0x%04x",
+					EventIdGetNbInRun(dateHeader.EventId), fFecId, (nextFThm & 0x0ffff), FT)
+				logger.Error(errMessage.Error())
+				break
+			}
+			position = decodeCharge(data, position, wfPointers, chPositions, uint32(time))
+		}
+	}
+}
+
+func decodeChargeIndiaFiberCompressed(data []uint16, position int, waveforms []*[]int16,
+	current_bit *int, huffman *HuffmanNode, channelMask []uint16, time uint32) int {
+	var dataword uint32 = 0
+
+	for _, channelID := range channelMask {
+		if *current_bit < 16 {
+			position++
+			*current_bit += 16
+		}
+		// Pack two 16-bit words into a 32-bit word in the correct order
+		dataword = (uint32(data[position]) << 16) | uint32(data[position+1])
+
+		// Get previous value
+		waveform := *waveforms[channelID%100]
+		var previous int16 = 0
+		if time > 0 {
+			previous = waveform[time-1]
+		}
+
+		var control_code int32 = 123456
+		wfvalue := int16(decode_compressed_value(int32(previous), dataword, control_code, current_bit, huffman))
+
+		if configuration.Verbosity > 3 {
+			message := fmt.Sprintf("Fiber ElecID %d, time %d, charge 0x%04x", channelID, time, wfvalue)
+			logger.Info(message, "fibers")
+		}
+
+		waveform[time] = wfvalue
+	}
+	return position
+}
+
+func computeFiberWaveformPointerArray(waveforms map[uint16][]int16, chmask []uint16, positions []uint16) []*[]int16 {
+	MAX_FIBERS_PER_FEC := 12
+	wfPointers := make([]*[]int16, MAX_FIBERS_PER_FEC)
+	for i, elecID := range chmask {
+		position := positions[i]
+		wf := waveforms[elecID]
+		wfPointers[position] = &wf
+	}
+	return wfPointers
+}
+
+func fibersChannelMask(evtFormat *EventFormat) ([]uint16, []uint16) {
+	var elecID uint16
+
+	channelMaskVec := make([]uint16, 0)
+	// To avoid using the map for every waveform sample we are keeping another
+	// vector with the pointers to the waveforms. This positions vector indicates
+	// the position of the waveform in the waveforms pointer array.
+	positions := make([]uint16, 0)
+
+	var t uint16
+	for t = 0; t < 16; t++ {
+		active := CheckBit(evtFormat.ChannelMask, t)
+		if active {
+			elecID = computeFiberElecID(evtFormat.FecID, t, evtFormat.FWVersion)
+			channelMaskVec = append(channelMaskVec, elecID)
+			positions = append(positions, computeFiberPosition(elecID))
+		}
+	}
+
+	if configuration.Verbosity > 2 {
+		message := fmt.Sprintf("Fiber channel mask: %v", channelMaskVec)
+		logger.Info(message, "fibers")
+	}
+	return channelMaskVec, positions
+}
+
+func computeFiberPosition(elecID uint16) uint16 {
+	position := elecID % 100 / 2
+	return position
+}
+
+func computeFiberElecID(fecID uint16, channel uint16, fwversion uint16) uint16 {
+	var elecID uint16
+
+	if fwversion >= 10 {
+		// Same logic as PMTs
+		elecID = channel*2 + (fecID % 2)
+		elecID += (((fecID - 2) / 4) + 1) * 100
+	}
+
+	return elecID
+}
+
+func writeFiberPedestals(evtFormat *EventFormat, channelMask []uint16, baselines map[uint16]uint16) {
+	for _, elecID := range channelMask {
+		// Same 12-to-6 baseline mapping as PMTs
+		baseline_index := ((elecID % 100) % 12) / 2
+		baselines[elecID] = evtFormat.Baselines[baseline_index]
+	}
+}
+
+func processFiberIds(event *EventType, configuration Configuration) {
+	if configuration.Verbosity > 2 {
+		message := fmt.Sprintf("processFiberIds: Initial FibersLG count=%d, DualMode=%t, ChannelsHG=%t",
+			len(event.FibersLG), event.PmtConfig.DualMode, event.PmtConfig.ChannelsHG)
+		logger.Info(message, "fibers")
+	}
+
+	// Iterate over all fiber waveforms read into FibersLG
+	for elecID, waveform := range event.FibersLG {
+		// Dual channel mode
+		// Channels 112-123, 212-223, etc. -> High Gain
+		if event.PmtConfig.DualMode {
+			if elecID%100 >= 12 {
+				newid := elecID - 12
+				event.FibersHG[newid] = waveform
+				event.FiberBaselinesHG[newid] = event.FiberBaselinesLG[elecID]
+				delete(event.FibersLG, elecID)
+				delete(event.FiberBaselinesLG, elecID)
+			}
+		}
+
+		// High-low gain channels
+		// Odd channels (101, 103, 105, ...) -> High Gain
+		if event.PmtConfig.ChannelsHG {
+			if elecID%2 == 1 {
+				event.FibersHG[elecID] = waveform
+				event.FiberBaselinesHG[elecID] = event.FiberBaselinesLG[elecID]
+				delete(event.FibersLG, elecID)
+				delete(event.FiberBaselinesLG, elecID)
+			}
+		}
+	}
+
+	if configuration.Verbosity > 2 {
+		message := fmt.Sprintf("processFiberIds: Final FibersLG count=%d, FibersHG count=%d",
+			len(event.FibersLG), len(event.FibersHG))
+		logger.Info(message, "fibers")
+	}
+}
